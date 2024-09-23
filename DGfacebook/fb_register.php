651618997e920c71b5d9f855a3bdb71190b98a3f
Daniel <139935112+Daniel-dgyang@users.noreply.github.com>
2024-09-24 02:03:29 +0900

    Update fb_register.php

--- DGfacebook/fb_register.php
+++ DGfacebook/fb_register.php
@@ -116,7 +116,7 @@
         </div>
       </div>
       <div id="footer">
-        <p>대표 번호: +82)10-3501-1047 E-MAIL: were2019@gmail.com</p>
+        <p>대표 번호: +82)10-xxxx-xxxxE-MAIL: xxxx123@xxx.com</p>
         <p style="margin-top: -10px">© Copyright DGfacebook. All Rights Reserved</p>
       </div>
     </div>
